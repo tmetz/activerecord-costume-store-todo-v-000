2cc223518a91ee8d84b62028ccc1fd104d059ed8
Tammy Metz <tammy@tammymetz.com>
2018-09-30 15:51:11 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,7 @@
 
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
-    create_table :haunted_stores do |t|
+    create_table :haunted_houses do |t|
       t.string :name
       t.string :location
       t.string :theme
